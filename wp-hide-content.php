e546a989fe5504b865e5f9f5521d181ba3bce88c
pier <pierluigi.tonegato@gmail.com>
2016-10-12 00:50:51 +0200

    jbhjh

--- wp-hide-content.php
+++ wp-hide-content.php
@@ -9,3 +9,26 @@ Author URI:https://github.com/Tpierluigi
 */
 
 require_once "admin.php";
+class hide_cat{
+    static function filtro($categories){
+        foreach ( $categories as $id=>$cat ){
+            //if ($cat->cat_ID %2 == 0 ){
+                $categories[$id]['cat_name'] = "dispari!!";
+            //}
+        }
+        echo_log("boooh");
+        return $categories;
+    }
+    static function prova($ris){
+        return $ris . "<h1>ciao</h1>";
+    }
+}
+add_filter('get_categories',array('hide_cat','filtro'));
+add_filter('wp_list_categories',array('hide_cat','prova'));
+/* Echo variable
+ * Description: Uses <pre> and print_r to display a variable in formated fashion
+ */
+function echo_log( $what )
+{
+    echo '<pre>'.print_r( $what, true ).'</pre>';
+}
